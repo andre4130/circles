b64c31500f80753df449d3e28d59f1c0d31e95c5
andre <andre4130@gmail.com>
2021-01-10 20:18:45 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import Circle from './components/circle/Circle';
+
 
 function App() {
+
+  const [text, setText] = useState("Circle Name");
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid d-flex p-0 app">
+        <Circle text={text}/>
     </div>
   );
 }
